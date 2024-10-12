1ede33d28f89509fcbc6248f9a499206cd668b0c
Pranav Bhat <pranavbhat2004@gmail.com>
2024-10-12 20:42:34 +0530

    update: server side code

--- server/main.py
+++ server/main.py
@@ -10,7 +10,7 @@ app  = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://mage-ai-3t91.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
