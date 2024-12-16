c18d0ac4923a478c36868f3bb8f53cc904278ed0
Pranav Bhat <pranavbhat2004@gmail.com>
2024-12-16 12:40:08 +0530

    add: cors handling on next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/:path*", // Match all routes
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-CSRF-Token, X-Requested-With" },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
